9793a0e0f88386de5e0d1f58a99393faf3fd9008
se7777en <95012661+se7777en@users.noreply.github.com>
2022-03-20 18:17:48 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let counter = 0;
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+document.addEventListener("keydown", function (event) {
+    jump();
+    counter++;
+    if(counter < 0) {counter = 0;}
+    console.log(counter);
+    
+});
+
+function jump() {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump");
+        
+    }
+    setTimeout(function () {
+        
+        dino.classList.remove("jump");
+    }, 300);
+
+}
+
+
+
+let isAlive = setInterval(function(){
+let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+
+if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+    alert("GAME OVER!!");
+    counter --;
+    if(counter < 0) {counter = 0;}
+    console.log(counter);
+}
+},10);
+
